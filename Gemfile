4a4793428a594fd918813cadbb241ddc4cdef967
romain hallary <niamor@mm.st>
2017-11-08 15:44:11 +0000

    added edit and delete on reviews

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
 gem 'jquery-rails'
+
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Hipster.word,
+    address: Faker::Address.street_address + ", " +  Faker::Address.city,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+
+  rand(2..10).times do
+    Review.create(restaurant: restaurant, content: Faker::Hipster.sentences(1), rating: rand(1..5))
+  end
+end
