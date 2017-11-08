e2a5672113a87380c8a888573d50b78add4b6326
romain hallary <niamor@mm.st>
2017-11-08 11:22:39 +0000

    added models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'reviews/show'
+
+  get 'reviews/edit'
+
+  get 'reviews/update'
+
+  get 'reviews/destroy'
+
+  get 'restaurants/index'
+
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
+  get 'restaurants/show'
+
+  get 'restaurants/edit'
+
+  get 'restaurants/update'
+
+  get 'restaurants/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
